eba7f589e26e41b6e7420c20d8542867412f67d9
Jonathan Halterman <jhalterman@gmail.com>
2016-10-07 20:05:15 -0700

    Fixed a few import statements

--- src/main/java/com/eucalyptus/tests/awssdk/TestSTSConditionKeys.groovy
+++ src/main/java/com/eucalyptus/tests/awssdk/TestSTSConditionKeys.groovy
@@ -17,10 +17,10 @@ import com.amazonaws.services.securitytoken.AWSSecurityTokenServiceClient
 import com.github.sjones4.youcan.youare.YouAre
 import com.github.sjones4.youcan.youare.YouAreClient
 import org.testng.annotations.Test
-import static src.main.java.com.eucalyptus.tests.awssdk.N4j.minimalInit
-import static src.main.java.com.eucalyptus.tests.awssdk.N4j.CLC_IP
-import static src.main.java.com.eucalyptus.tests.awssdk.N4j.ACCESS_KEY
-import static src.main.java.com.eucalyptus.tests.awssdk.N4j.SECRET_KEY
+import static com.eucalyptus.tests.awssdk.N4j.minimalInit
+import static com.eucalyptus.tests.awssdk.N4j.CLC_IP
+import static com.eucalyptus.tests.awssdk.N4j.ACCESS_KEY
+import static com.eucalyptus.tests.awssdk.N4j.SECRET_KEY
 
 import java.text.SimpleDateFormat
 
