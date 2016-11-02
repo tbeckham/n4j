bd285523e72a6ae5002581a5396090052f3b4de1
Lincoln Thomas <lincoln.thomas@hpe.com>
2016-11-02 10:50:44 -0700

    EUCA-12851 Re-org and change test CORS config to prepare for testing cross-origin requests

--- src/main/java/com/eucalyptus/tests/awssdk/S3CorsTests.java
+++ src/main/java/com/eucalyptus/tests/awssdk/S3CorsTests.java
@@ -34,19 +34,11 @@ import com.amazonaws.services.s3.model.Owner;
 
 /**
  * <p>
- * This class contains tests for getting, setting, verifying, and deleting
- * rules for Cross-Origin Resource Sharing (CORS) on a bucket.
+ * Test the Cross-Origin Resource Sharing (CORS) feature.
+ * 
+ * The preflight OPTIONS requests are tested separately in "cors_tests.py" 
+ * in the nephoria python test suite.
  * <p>
- * It also tests pre-flight requests that normally come from a Web browser
- * to determine what resources the browser can request from other origins.
- * <p>
- * Since CORS is not yet implemented by Eucalyptus, and the tests expect 
- * a 501 NotImplemented error response, these tests will pass for Eucalyptus
- * and fail for AWS. 
- * </p>
- * The next incarnation of these tests will switch to expecting valid
- * responses, and will thus pass for AWS, and fail for Eucalyptus until the
- * CORS features is added.
  *
  * @author Lincoln Thomas <lincoln.thomas@hpe.com>
  * 
@@ -124,17 +116,70 @@ public class S3CorsTests {
     }
   }
 
+  private int NUM_CONFIG_RULES = 4;
+  
+  private static BucketCrossOriginConfiguration createCorsConfig() {
+    /**
+     * Create a CORS configuration of several rules, based on the examples in:
+     * http://docs.aws.amazon.com/AmazonS3/latest/dev/cors.html
+     */
+    List<CORSRule> corsRuleListCreated = new ArrayList<CORSRule>(4);
+
+    CORSRule corsRuleExample1Writes = new CORSRule();
+    corsRuleExample1Writes.setId("Rule 1: Origin example1 can write, with all headers allowed");
+    corsRuleExample1Writes.setAllowedOrigins("http://www.example1.com");
+    corsRuleExample1Writes.setAllowedMethods(
+        AllowedMethods.PUT, 
+        AllowedMethods.POST, 
+        AllowedMethods.DELETE);
+    corsRuleExample1Writes.setAllowedHeaders("*");
+    corsRuleListCreated.add(corsRuleExample1Writes);
+
+    CORSRule corsRuleExample2Reads = new CORSRule();
+    corsRuleExample2Reads.setId("Rule 2: Origin example2 can GET only");
+    corsRuleExample2Reads.setAllowedOrigins("http://www.example2.com");
+    corsRuleExample2Reads.setAllowedMethods(AllowedMethods.GET);
+    corsRuleListCreated.add(corsRuleExample2Reads);
+
+    CORSRule corsRuleAnyHeads = new CORSRule();
+    corsRuleAnyHeads.setId("Rule 3: Any origin can HEAD");
+    corsRuleAnyHeads.setAllowedOrigins("*");
+    corsRuleAnyHeads.setAllowedMethods(AllowedMethods.HEAD);
+    corsRuleListCreated.add(corsRuleAnyHeads);
+
+    CORSRule corsRuleComplex = new CORSRule();
+    corsRuleComplex.setId(
+        "Rule 4: Either of these wildcarded origins can do any method, " +
+        "can cache the response for 50 minutes, " +
+        "can only send request headers that begin x-amz- or Content-, " +
+        "and can expose the listed ExposeHeaders to clients.");
+    corsRuleComplex.setAllowedOrigins("http://www.corstest*.com", "http://*.sample.com");
+    corsRuleComplex.setAllowedMethods(
+        AllowedMethods.GET,
+        AllowedMethods.HEAD,
+        AllowedMethods.PUT, 
+        AllowedMethods.POST, 
+        AllowedMethods.DELETE);
+    corsRuleComplex.setMaxAgeSeconds(3000);
+    corsRuleComplex.setAllowedHeaders(
+        "x-amz-*", 
+        "Content-*");
+    corsRuleComplex.setExposedHeaders(
+        "x-amz-server-side-encryption",
+        "x-amz-request-id",
+        "x-amz-id-2");
+    corsRuleListCreated.add(corsRuleComplex);
+
+    return new BucketCrossOriginConfiguration(corsRuleListCreated);
+  }
+  
   /**
    * Test getting, setting, verifying, and deleting
    * rules for Cross-Origin Resource Sharing (CORS) on a bucket.
-   * <p>
-   * Test pre-flight requests that normally come from a Web browser
-   * to determine what resources the browser can request from other origins.
-   * <p>
    */
   @Test
-  public void testCors() throws Exception {
-    testInfo(this.getClass().getSimpleName() + " - testCors");
+  public void testCorsConfigMgmt() throws Exception {
+    testInfo(this.getClass().getSimpleName() + " - testCorsConfigMgmt");
 
     try {
       print(account + ": Fetching empty bucket CORS config for " + bucketName);
@@ -150,39 +195,7 @@ public class S3CorsTests {
 
     try {
       print(account + ": Setting bucket CORS config for " + bucketName);
-      /**
-       * Create a CORS configuration of several rules, based on the examples in:
-       * http://docs.aws.amazon.com/AmazonS3/latest/dev/cors.html
-       */
-      List<CORSRule> corsRuleListCreated = new ArrayList<CORSRule>(2);
-
-      CORSRule corsRuleGets = new CORSRule();
-      corsRuleGets.setAllowedOrigins("*");
-      corsRuleGets.setAllowedMethods(AllowedMethods.GET);
-      corsRuleListCreated.add(corsRuleGets);
-
-      CORSRule corsRulePuts = new CORSRule();
-      corsRulePuts.setAllowedOrigins("https", "http://*.example1.com", "http://www.example2.com");
-      corsRulePuts.setAllowedMethods(
-          AllowedMethods.PUT, 
-          AllowedMethods.POST, 
-          AllowedMethods.DELETE);
-      corsRulePuts.setAllowedHeaders("*");
-      corsRuleListCreated.add(corsRulePuts);
-
-      CORSRule corsRuleExtended = new CORSRule();
-      corsRuleExtended.setAllowedOrigins("*");
-      corsRuleExtended.setAllowedMethods(AllowedMethods.GET);
-      corsRuleExtended.setAllowedHeaders("*");
-      corsRuleExtended.setId("ManuallyAssignedId1");
-      corsRuleExtended.setMaxAgeSeconds(3000);
-      corsRuleExtended.setExposedHeaders(
-          "x-amz-server-side-encryption",
-          "x-amz-request-id",
-          "x-amz-id-2");
-      corsRuleListCreated.add(corsRuleExtended);
-
-      BucketCrossOriginConfiguration corsConfigCreated = new BucketCrossOriginConfiguration(corsRuleListCreated);
+      BucketCrossOriginConfiguration corsConfigCreated = createCorsConfig();
       s3.setBucketCrossOriginConfiguration(bucketName, corsConfigCreated);
       
     } catch (AmazonServiceException ase) {
@@ -191,8 +204,9 @@ public class S3CorsTests {
     }
 
     try {
-      //LPT Is there an async delay between when setting a CORS config returns to the
-      // caller, and when it's available for a Get? Delay to test that.
+      //TODO Cases seen where there's a delay between when Setting a CORS config returns to the
+      // caller, and when it's available for a Get. Might only be a problem with AWS, not Euca.
+      // Test this more, against both, with different timeouts, and investigate Euca code.
       Thread.sleep(10000);
       
       print(account + ": Fetching populated bucket CORS config for " + bucketName);
@@ -200,64 +214,66 @@ public class S3CorsTests {
       assertTrue("No CORS config retrieved.", corsConfigRetrieved != null);
 
       List<CORSRule> corsRuleListRetrieved = corsConfigRetrieved.getRules();
-      assertTrue("Expected to receive a CORS config of 3 rules. Returned corsConfig has " + 
+      assertTrue("Expected to receive a CORS config of " + NUM_CONFIG_RULES + " rules. " + 
+          "Returned corsConfig has " + 
           corsRuleListRetrieved.size() + " rules.", 
-          corsRuleListRetrieved.size() == 3);
+          corsRuleListRetrieved.size() == NUM_CONFIG_RULES);
 
-      // Find the only rule with an ID, and check its fields
+      // Check the rule fields of the complex last rule
 
       int ruleSequence = 0;
+      boolean ruleFound = false;
       for (CORSRule corsRuleRetrieved : corsRuleListRetrieved ) {
-          ruleSequence++;
+        ruleSequence++;
 
-          assertTrue("Received a null CORS rule in the retrieved CORS configuration",
-                     corsRuleListRetrieved != null);
+        assertTrue("Received a null CORS rule in the retrieved CORS configuration",
+            corsRuleListRetrieved != null);
 
-          String ruleIdReceived = corsRuleRetrieved.getId();
-          if (ruleIdReceived != null &&
-              ruleIdReceived.equals("ManuallyAssignedId1")) {
+        String ruleIdReceived = corsRuleRetrieved.getId();
+        if (ruleIdReceived != null &&
+            ruleIdReceived.startsWith("Rule 4")) {
+          ruleFound = true;
 
-              // It should be the 3rd CORS rule
-              assertTrue("Rule found is out of sequence, should be 3, is: " + ruleSequence,
-                         ruleSequence == 3);
+          // It should be the 4th CORS rule
+          assertTrue("Rule found is out of sequence, should be 4, is: " + ruleSequence,
+              ruleSequence == 4);
 
-              List<String> originsReceived = corsRuleRetrieved.getAllowedOrigins();
-              assertTrue("Allowed Origin is unexpected: " + originsReceived, 
-                         originsReceived != null && originsReceived.size() == 1 &&
-                         originsReceived.get(0).equals("*"));
+          List<String> originsReceived = corsRuleRetrieved.getAllowedOrigins();
+          assertTrue("Allowed Origin is unexpected: " + originsReceived, 
+              originsReceived != null && originsReceived.size() == 2 &&
+              originsReceived.get(1).equals("http://*.sample.com"));
 
-              List<CORSRule.AllowedMethods> methodsReceived = corsRuleRetrieved.getAllowedMethods();
-              assertTrue("Allowed Methods is unexpected: " + methodsReceived, 
-                         methodsReceived != null && methodsReceived.size() == 1 &&
-                         methodsReceived.get(0).equals(CORSRule.AllowedMethods.GET));
+          List<CORSRule.AllowedMethods> methodsReceived = corsRuleRetrieved.getAllowedMethods();
+          assertTrue("Allowed Methods is unexpected: " + methodsReceived, 
+              methodsReceived != null && methodsReceived.size() == 5 &&
+              methodsReceived.get(4).equals(CORSRule.AllowedMethods.DELETE));
 
-              List<String> allowedHeadersReceived = corsRuleRetrieved.getAllowedHeaders();
-              assertTrue("Allowed Headers is unexpected: " + allowedHeadersReceived, 
-                         allowedHeadersReceived != null && allowedHeadersReceived.size() == 1 &&
-                         allowedHeadersReceived.get(0).equals("*"));
+          int maxAgeReceived = corsRuleRetrieved.getMaxAgeSeconds();
+          assertTrue("Max Age in Seconds is unexpected: " + maxAgeReceived,
+              maxAgeReceived == 3000);
 
-              int maxAgeReceived = corsRuleRetrieved.getMaxAgeSeconds();
-              assertTrue("Max Age in Seconds is unexpected: " + maxAgeReceived,
-                         maxAgeReceived == 3000);
+          List<String> allowedHeadersReceived = corsRuleRetrieved.getAllowedHeaders();
+          assertTrue("Allowed Headers is unexpected: " + allowedHeadersReceived, 
+              allowedHeadersReceived != null && allowedHeadersReceived.size() == 2 &&
+              allowedHeadersReceived.get(0).equals("x-amz-*"));
 
-              ArrayList<String> exposedHeadersExpected = new ArrayList<String>(3);
-              exposedHeadersExpected.add("x-amz-server-side-encryption");
-              exposedHeadersExpected.add("x-amz-request-id");
-              exposedHeadersExpected.add("x-amz-id-2");
-              List<String> exposedHeadersReceived = corsRuleRetrieved.getExposedHeaders();
-              assertTrue("Exposed Headers is unexpected: " + exposedHeadersReceived, 
-                         exposedHeadersReceived != null && 
-                         exposedHeadersReceived.size() == exposedHeadersExpected.size() &&
-                         exposedHeadersReceived.containsAll(exposedHeadersExpected));
+          ArrayList<String> exposedHeadersExpected = new ArrayList<String>(3);
+          exposedHeadersExpected.add("x-amz-server-side-encryption");
+          exposedHeadersExpected.add("x-amz-request-id");
+          exposedHeadersExpected.add("x-amz-id-2");
+          List<String> exposedHeadersReceived = corsRuleRetrieved.getExposedHeaders();
+          assertTrue("Exposed Headers is unexpected: " + exposedHeadersReceived, 
+              exposedHeadersReceived != null && 
+              exposedHeadersReceived.size() == exposedHeadersExpected.size() &&
+              exposedHeadersReceived.containsAll(exposedHeadersExpected));
           } //end if this is the rule we validate
       } //end for all rules retrieved
+      assertTrue("Did not find the complex CORS rule to validate in the retrieved CORS config.", ruleFound);
     } catch (AmazonServiceException ase) {
       printException(ase);
       assertTrue("Caught AmazonServiceException trying to get the bucket CORS config: " + ase.getMessage(), false);
     }
 
-    // Preflight OPTIONS requests tests implemented in nephoria (python) instead.
-    
     try {
       print(account + ": Deleting bucket CORS config for " + bucketName);
       s3.deleteBucketCrossOriginConfiguration(bucketName);
@@ -278,8 +294,32 @@ public class S3CorsTests {
       assertTrue("Caught AmazonServiceException trying to get the empty (deleted) bucket CORS config: " + ase.getMessage(), false);
     }
 
-  }
+  }  // end testCorsConfigMgmt()
+  
+  
+  /**
+   * Test cross-origin requests, which contain an "Origin" HTTP header.
+   * Verify the CORS-specific response headers (if any).
+   * Test various S3 bucket and object operations. Any S3 operation could be
+   * sent as a cross-origin request.
+   */
+  @Test
+  public void testCorsRequests() throws Exception {
+    testInfo(this.getClass().getSimpleName() + " - testCorsRequests");
 
+    try {
+      print(account + ": Setting bucket CORS config for " + bucketName);
+      BucketCrossOriginConfiguration corsConfigCreated = createCorsConfig();
+      s3.setBucketCrossOriginConfiguration(bucketName, corsConfigCreated);
+    } catch (AmazonServiceException ase) {
+      printException(ase);
+      assertTrue("Caught AmazonServiceException trying to set the bucket CORS config: " + ase.getMessage(), false);
+    }
+
+
+  }  // end testCorsRequests()
+  
+  
   private void printException(AmazonServiceException ase) {
     ase.printStackTrace();
     print("Caught Exception: " + ase.getMessage());
